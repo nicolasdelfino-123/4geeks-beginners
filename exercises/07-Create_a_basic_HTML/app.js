1dd87c944ca4bea30143e406544e175b79a3d2c4
Nicolas <nicolasdelfino585@gmail.com>
2025-03-25 13:30:47 -0300

    repaso de ejercicios done

--- exercises/07-Create_a_basic_HTML/app.js
+++ exercises/07-Create_a_basic_HTML/app.js
@@ -1,13 +1,13 @@
-const a = '</title>';
-const b = '</html>';
-const c = '<head>';
-const d = '</body>';
-const e = '<html>';
-const f = '</head>';
-const g = '<title>';
-const h = '<body>';
+const a = "</title>";
+const b = "</html>";
+const c = "<head>";
+const d = "</body>";
+const e = "<html>";
+const f = "</head>";
+const g = "<title>";
+const h = "<body>";
 
 //Modify this variable
-let htmlDocument = 'e+c+g+h+d+a+f+b';
+let htmlDocument = `${e}${c}${g}${a}${f}${h}${d}${b}`;
 
 console.log(htmlDocument);
--- exercises/20-Looping_with_FizzBuzz/app.js
+++ exercises/20-Looping_with_FizzBuzz/app.js
@@ -1,23 +1,14 @@
-/* Escribe el código necesario para imprimir en la consola los números del 1 al 100.
-
-Para múltiplos de 3, en vez del número, imprime la palabra Fizz.
-
-Para múltiplos de 5, imprime la palabra Buzz.
-
-Para números que sean múltiplos de 3 y 5, imprime FizzBuzz. */
-
 function fizzBuzz() {
   // Your code here
-
-  for (let num = 1; num <= 100; num++) {
-    if (num % 3 === 0 && num % 5 === 0) {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
       console.log("FizzBuzz");
-    } else if (num % 5 === 0) {
+    } else if (i % 5 === 0) {
       console.log("Buzz");
-    } else if (num % 3 === 0) {
+    } else if (i % 3 === 0) {
       console.log("Fizz");
     } else {
-      console.log(num);
+      console.log(i);
     }
   }
 }
